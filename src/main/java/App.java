bc8753804eab6dedce2287db844d9e6b45466ea3
dmden22 <maslovden22@yandex.ru>
2023-11-16 11:04:00 +0300

    2.1.3 - done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("getCat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("getCat");
+        beanCat1.catSay();
+        beanCat2.catSay();
+
+        System.out.println("Бины HelloWorld указывают на один и тот же объект? : " + (bean == bean2));
+        System.out.println("Бины Cat указывают на один и тот же объект? : " + (beanCat1 == beanCat2));
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,5 @@
+public class Cat {
+    public void catSay() {
+        System.out.println("Myau");
+    }
+}
